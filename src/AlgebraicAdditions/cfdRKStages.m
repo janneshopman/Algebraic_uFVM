ab98da7fcc25a3e354c7326916e8e7d9604a8da7
janneshopman <77000222+janneshopman@users.noreply.github.com>
2023-11-16 12:04:36 +0100

    Update cfdRKStages.m

--- src/AlgebraicAdditions/cfdRKStages.m
+++ src/AlgebraicAdditions/cfdRKStages.m
@@ -18,15 +18,15 @@ p = cfdGetField('p');
 U = cfdGetField('U');
 Uf = cfdGetField('Uf');
 
-%reset for RK scheme
+% Reset for RK scheme
 UOld = U;
 pOld = p;
 dUs = zeros(size(U, 1), RK.nStages);
 
 for iStage = 1:RK.nStages + 1
-    %stage increment of U
-    %Is there a better way to include a pressure prediction?
-    %Perhaps using updated intermediate values of pressure?
+    % Stage increment of U
+    % Is there a better way to include a pressure prediction?
+    % Perhaps using updated intermediate values of pressure?
     U = UOld + dUs*RK.aTab(iStage, :)'; % - sum(RK.aTab(iStage,:)) * deltaT * pnPredCoef * op.Gc * pOld (3)
     cfdBCUpdate(U, 'U');
 
@@ -34,18 +34,18 @@ for iStage = 1:RK.nStages + 1
 
         divU = cfdGetInternalField(op.Mc * U, 'vsf');
 
-        %Intermediate p used for predictor, is it optimal?
+        % Intermediate p used for predictor, is it optimal?
         dtp = deltaT * cfdGetInternalField(p, 'vsf');
 
-        %Before Poisson equation, b has to be updated according to bc (1)
-        %And reference pressure has to be set (2)
-        %Include preconditioner (4)
+        % Before Poisson equation, b has to be updated according to bc (1)
+        % And reference pressure has to be set (2)
+        % Include preconditioner (4)
         [dtp, flag, relres, iter, resvec] = pcg(-op.Lap, -divU, sol.tolerance, sol.maxIter, speye(size(dtp, 1)), speye(size(dtp, 1)), dtp);
 
         p = cfdSetInternalField(p, dtp/deltaT, 'vsf');
         p = cfdBCUpdate(p, 'p');
 
-        %velocity update
+        % Velocity update
         U = U - deltaT * op.Gc * p;
         U = cfdBCUpdate(U, 'U');
 
@@ -55,20 +55,20 @@ for iStage = 1:RK.nStages + 1
             Uf = op.GamCS*U;
         end
 
-        %Experiment with mixed Laplacian
-        %Us = op.GamCS*U+op.LMix*(op.GamCS*op.GamSC - speye(geo.nf))*op.G * dtp; (5)
+        % Experiment with mixed Laplacian
+        % Us = op.GamCS*U+op.LMix*(op.GamCS*op.GamSC - speye(geo.nf))*op.G * dtp; (5)
     end
 
     if iStage <= RK.nStages
-        %set convective
+        % Set convective
         Con = kron(eye(3), op.M*spdiags(Uf, 0, theNumberOfFaces, theNumberOfFaces)*op.PiCSM);
 
-        %set delta U for this stage
+        % Set delta U for this stage
         dUs(:, iStage) = -deltaT * (op.Omega\((Con - nu * kron(eye(3), op.L))*U));
     end
 end
 
-%p = pnPredCoef*pOld + p; (3)
+% p = pnPredCoef*pOld + p; (3)
 
 cfdSetField(p, 'p');
 cfdSetField(U, 'U');
