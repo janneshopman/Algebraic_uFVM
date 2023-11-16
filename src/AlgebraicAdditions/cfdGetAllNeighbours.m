f127a382672f66e861bfefc792a4b573d3cd13cf
janneshopman <77000222+janneshopman@users.noreply.github.com>
2023-11-16 11:58:04 +0100

    Update cfdGetAllNeighbours.m

--- src/AlgebraicAdditions/cfdGetAllNeighbours.m
+++ src/AlgebraicAdditions/cfdGetAllNeighbours.m
@@ -1,12 +1,4 @@
 function allNeighbours = cfdGetAllNeighbours
-%--------------------------------------------------------------------------
-%
-%  Written by the CFD Group @ AUB, Fall 2017
-%  Contact us at: cfd@aub.edu.lb
-%==========================================================================
-% Routine Description:
-%   
-%--------------------------------------------------------------------------
 
 global Region;
 
